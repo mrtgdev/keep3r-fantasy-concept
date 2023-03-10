db6f7fae2c189321f4acdd31c3c79bdd5684936e
mrtgdev <mrtg.dev@outlook.com>
2023-03-10 14:38:19 +0100

    vite > assets redireccionados, habrá futuros cambios

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,9 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: 'src',
   build: { 
-    outDir: 'dist' 
+    outDir: '../dist' 
   },
   plugins: [
     preact(), tsconfigPaths(),
@@ -16,7 +17,7 @@ export default defineConfig({
       mode: 'development',
       base: '/',
       scope: '/',
-      includeAssets: ['favicon.svg'],
+      includeAssets: ['/favicon.svg'],
       registerType: 'autoUpdate',
       devOptions: {
         enabled: true,
@@ -28,19 +29,17 @@ export default defineConfig({
           theme_color: '#080808',
           icons: [
               {
-                  src: 'pwa-192x192.png',
+                  src: './assets/images/pwa-192x192.png',
                   sizes: '192x192',
                   type: 'image/png',
-                  purpose: 'any maskable'
               },
               {
-                  src: 'pwa-512x512.png',
+                  src: './assets/images/pwa-512x512.png',
                   sizes: '512x512',
                   type: 'image/png',
-                  purpose: 'any maskable'
               },
               {
-                  src: 'pwa-512x512.png',
+                  src: './assets/images/pwa-512x512.png',
                   sizes: '512x512',
                   type: 'image/png',
                   purpose: 'any maskable',
