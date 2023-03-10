f89f77904c0109102b810aad20207bb932718f49
mrtgdev <mrtg.dev@outlook.com>
2023-03-10 14:47:11 +0100

    button > se ajusta el magnet button

--- src/components/buttons/magnet.button.tsx
+++ src/components/buttons/magnet.button.tsx
@@ -9,19 +9,20 @@ export default function MagnetButton( props: { children: ComponentChild[] } ) {
     useEffect(() => {
         let magnetBtn = document.getElementById("magnet")
 
-        if ( magnetBtn ) {
-            magnetBtn.addEventListener( "mousemove", function( event ) {
-                let position = magnetBtn.getBoundingClientRect()
-                let x = event.pageX - position.left - magnetBtn.offsetWidth / 2
-                let y = event.pageY - position.top - magnetBtn.offsetHeight / 2
-                magnetBtn.children[0].style.transform = `translate(${x * 0.3}px, ${y * 0.6}px)`
+            magnetBtn?.addEventListener( "mousemove", function( event ) {
+                if ( magnetBtn ) {
+                    let position = magnetBtn.getBoundingClientRect()
+                    let x = event.pageX - position.left - magnetBtn.offsetWidth / 2
+                    let y = event.pageY - position.top - magnetBtn.offsetHeight / 2
+                    magnetBtn.children[0].style.transform = `translate(${x * 0.3}px, ${y * 0.6}px)`
+                }
             })
             
-            magnetBtn.addEventListener( "mouseout", function( event ){
-                magnetBtn.children[0].style.transform = `translate( 0px, 0px )`
+            magnetBtn?.addEventListener( "mouseout", function( event ){
+                if ( magnetBtn )
+                    magnetBtn.children[0].style.transform = `translate( 0px, 0px )`
             })
-        }
-    })
+        },[])
 
     return (
         <div id='magnet' class='relative w-full h-full justify-center items-center align-middle z-20 border-4 border-white'>
